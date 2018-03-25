ff3d780307766a24ff4695f749411e1fc2cedc89
Johannes Pfannmueller <johannes.pfannmueller@googlemail.com>
2018-03-25 19:18:59 +0200

    script to setup the schema on the local mysql server

---
+++ PythonWorkspace/Environment/setupSchema.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import mysql.connector
+db = mysql.connector.connect(user='root', password='root', host='localhost')
+cursor = db.cursor();
+try:
+    cursor.execute("""CREATE SCHEMA `student`;""");
+    db.commit();
+except:
+    db.rollback();
+db.close();
+
+db = mysql.connector.connect(user='root', password='root', host='localhost',database='student3')
+cursor = db.cursor();
+
+try:
+    cursor.execute("""CREATE TABLE `student`.`YouTubeDataset` (
+    `row_names` TEXT NULL,
+    `requestTime` DOUBLE NULL,
+    `userId` BIGINT(20) NULL,
+    `videoId` TEXT NULL,
+    `sessionDuration` DOUBLE NULL,
+    `avgChunkDuration` DOUBLE NULL,
+    `chunks` BIGINT(20) NULL,
+    `duration` TEXT NULL,
+    `uploaded` TEXT NULL,
+    `uploader` TEXT NULL,
+    `category` TEXT NULL,
+    `bitrate` BIGINT(20) NULL,
+    `index` INT(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`index`));""");
+    db.commit();
+except:
+    db.rollback();
+db.close();
